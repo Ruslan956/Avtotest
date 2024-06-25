7848551ee205a67dfbb116050521e4794e3b3199
Ruslan <148627758+Ruslan956@users.noreply.github.com>
2024-06-25 07:36:40 +0500

    Update Автотест для поиска.py

--- Автотест для поиска.py
+++ Автотест для поиска.py
@@ -5,11 +5,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 from time import sleep
+
+# Создаем экземпляр драйвера браузера
 driver = webdriver.Chrome()
+
+# Открываем сайт samokat.ru
 url = "https://samokat.ru/"
 driver.get(url)
+
+# Находим поле для поиска по классам
 search_string = driver.find_element(By.CLASS_NAME,"_input_184dk_25")
+
+# Вводим  слово"вода" в строку поиска 
 search_string.send_keys("вода")
+
+# Ждем несколько секунд для закгрузки результатов поиска
 sleep(5)
 
 
